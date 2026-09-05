ee48e2bf36da85ba1db561bb0fdf7dfdbbfaa31e
James Stockwell <james.stockwell@ntlworld.com>
2026-09-05 22:56:00 +0100

    Camera follow

--- Assets/Resources/Script/LumiController.cs
+++ Assets/Resources/Script/LumiController.cs
@@ -22,6 +22,10 @@ public class Script : EntityController
             moveDirection.Set(move.x, move.y);
       	    moveDirection.Normalize();
         }
+        Debug.Log("Move X: " + move.x + "  Move Y: " + move.y);
+        animator.SetFloat("Move X", moveDirection.x);
+        animator.SetFloat("Move Y", moveDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
     }
 
     void FixedUpdate()
@@ -33,11 +37,7 @@ public class Script : EntityController
             rigidbody2d.MovePosition(rigidbody2d.position + knockbackDirection * knockbackForce * Time.fixedDeltaTime);
             return;
         }*/
-        
-        animator.SetFloat("Move X", move.x);
-        animator.SetFloat("Move Y", move.y);
-        rigidbody2d.linearVelocity = new Vector2(move.x, move.y) * speed;
-        //rigidbody2d.MovePosition(rigidbody2d.linearVelocity * Time.fixedDeltaTime);
-        //graphics.transform.localPosition = new Vector3(0, 0, 0);
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
